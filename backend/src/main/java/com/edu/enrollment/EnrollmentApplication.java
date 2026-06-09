259a1df73770ef9db9744381d4bba9393d5c35a0
cfsl456 <3028219613@qq.com>
2026-06-09 18:53:14 +0800

    20260609 修改注释

--- backend/src/main/java/com/edu/enrollment/EnrollmentApplication.java
+++ backend/src/main/java/com/edu/enrollment/EnrollmentApplication.java
@@ -3,7 +3,7 @@ package com.edu.enrollment;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//注释
+//注释1
 @SpringBootApplication
 @MapperScan("com.edu.enrollment.mapper")
 public class EnrollmentApplication {
