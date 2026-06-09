3708ccf46fc64f9ba257156a29293cc82a844e5b
zayron <441713552@qq.com>
2026-06-09 23:46:11 +0800

    20260609 修复了一些已知问题

--- backend/src/main/java/com/edu/enrollment/EnrollmentApplication.java
+++ backend/src/main/java/com/edu/enrollment/EnrollmentApplication.java
@@ -3,7 +3,7 @@ package com.edu.enrollment;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//注释
+
 @SpringBootApplication
 @MapperScan("com.edu.enrollment.mapper")
 public class EnrollmentApplication {
